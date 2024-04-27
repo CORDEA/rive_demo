330d2f059c33199ae07abd792d3cbdffa10128bb
Yoshihiro Tanaka <contact@cordea.jp>
2024-04-27 13:50:01 +0900

    simplify

---
+++ lib/home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home')),
+      body: const Center(
+        child: RiveAnimation.asset(
+          'assets/demo.riv',
+        ),
+      ),
+    );
+  }
+}
